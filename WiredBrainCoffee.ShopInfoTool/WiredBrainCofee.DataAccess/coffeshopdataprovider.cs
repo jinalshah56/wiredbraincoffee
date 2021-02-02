8d15048f9f930410ccdb70500b9ab201dbf5321c
jinalshah56 <59077326+jinalshah56@users.noreply.github.com>
2021-02-02 22:13:50 +0530

    added data for pull request

--- WiredBrainCoffee.ShopInfoTool/WiredBrainCofee.DataAccess/coffeshopdataprovider.cs
+++ WiredBrainCoffee.ShopInfoTool/WiredBrainCofee.DataAccess/coffeshopdataprovider.cs
@@ -12,8 +12,9 @@ namespace WiredBrainCofee.DataAccess
             yield return new coffeeshop { location = "ahmedabad", BeansInStockInKg = 108 };
             yield return new coffeeshop { location = "memdavad", BeansInStockInKg = 18 };
             yield return new coffeeshop { location = "abcd", BeansInStockInKg = 80 };
-            yield return new coffeeshop { location = "abcd", BeansInStockInKg = 809 };
-            yield return new coffeeshop { location = "abcd", BeansInStockInKg = 287 };
+            yield return new coffeeshop { location = "cd", BeansInStockInKg = 809 };
+            yield return new coffeeshop { location = "abd", BeansInStockInKg = 287 };
+            yield return new coffeeshop { location = "bcd", BeansInStockInKg = 230 };
 
         }
     }
