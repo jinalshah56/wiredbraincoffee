ff2d73f5b1d220dfe59a84e1f587e423ff7c97e0
jinalshah <jinalkshah1@gmail.com>
2021-02-02 21:54:37 +0530

    implement

---
+++ WiredBrainCoffee.ShopInfoTool/WiredBrainCofee.DataAccess/coffeshopdataprovider.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+using WiredBrainCofee.DataAccess.model;
+
+namespace WiredBrainCofee.DataAccess
+{
+    public class coffeshopdataprovider
+    {
+        public IEnumerable<coffeeshop> LoadCoffeeShops() 
+        {
+            yield return new coffeeshop { location = "ahmedabad", BeansInStockInKg = 108 };
+            yield return new coffeeshop { location = "memdavad", BeansInStockInKg = 18 };
+            yield return new coffeeshop { location = "abcd", BeansInStockInKg = 80 };
+        }
+    }
+}
---
+++ WiredBrainCoffee.ShopInfoTool/WiredBrainCofee.DataAccess/model/coffeeshop.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WiredBrainCofee.DataAccess.model
+{
+    public class coffeeshop
+    {
+        public string location { get; set; }
+
+        public int BeansInStockInKg { get; set; }
+    }
+}
